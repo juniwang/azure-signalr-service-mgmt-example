ff1ace89ae7fc661bf4f0378449a33736242d053
Junbo Wang <juniwang@microsoft.com>
2019-03-05 16:51:41 +0800

    Dotnet example

---
+++ dotnet/AzureSignalRMgmt/AzureSignalRMgmt/CredentialHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSignalRMgmt
+{
+    public class CredentialHelper
+    {
+        public static AzureCredentials GetServiceClientCredentials()
+        {
+            // see https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal about how to create a new SP
+            var clientId = ConfigurationManager.AppSettings["AzureSPClientId"];
+            var clientSecret = ConfigurationManager.AppSettings["AzureSPClientSecret"];
+            var tenantId = ConfigurationManager.AppSettings["TenantId"];
+            var subscriptionId = ConfigurationManager.AppSettings["SubscriptionId"];
+
+            var credential = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
+               clientId,
+               clientSecret,
+               tenantId,
+               AzureEnvironment.AzureGlobalCloud)
+               .WithDefaultSubscription(subscriptionId);
+
+            return credential;
+        }
+
+        public static IAzure GetAzure()
+        {
+            return Azure.Configure()
+                .Authenticate(GetServiceClientCredentials())
+                .WithSubscription(ConfigurationManager.AppSettings["SubscriptionId"]);
+        }
+    }
+}
---
+++ dotnet/AzureSignalRMgmt/AzureSignalRMgmt/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.SignalR;
+using Microsoft.Azure.Management.SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSignalRMgmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignalRManagementClient client = new SignalRManagementClient(CredentialHelper.GetServiceClientCredentials());
+            client.SubscriptionId = ConfigurationManager.AppSettings["SubscriptionId"];
+            var resourceGroup = SdkContext.RandomResourceName("rgTest", 20);
+            var region = Region.USWest;
+
+            // create ResourceGroup
+            var azure = CredentialHelper.GetAzure();
+            azure.ResourceGroups.Define(resourceGroup)
+                .WithRegion(region)
+                .Create();
+
+            try
+            {
+
+                // create SignalR
+                var signalRName = SdkContext.RandomResourceName("srTest", 20);
+                var parameter = new SignalRCreateParameters
+                {
+                    Location = "westus",
+                    Sku = new ResourceSku
+                    {
+                        Name = "Standard_S1",
+                        Tier = "Standard",
+                        Capacity = 1,
+                    },
+                    Properties = new SignalRCreateOrUpdateProperties { },
+                };
+
+                var sr = client.SignalR.CreateOrUpdate(resourceGroup, signalRName, parameter);
+                Console.WriteLine(sr.HostName);
+                Console.WriteLine(sr.ExternalIP);
+
+                // Scale
+                Console.Read();
+                var scale = new SignalRUpdateParameters
+                {
+                    Sku = new ResourceSku
+                    {
+                        Name = "Standard_S1",
+                        Capacity = 2,
+                    }
+                };
+                client.SignalR.Update(resourceGroup, signalRName, scale);
+                Console.WriteLine("updated");
+
+                // Delete
+                Console.Read();
+                client.SignalR.Delete(resourceGroup, signalRName);
+                Console.WriteLine("deleted");
+            }
+            finally
+            {
+                azure.ResourceGroups.DeleteByName(resourceGroup);
+                Console.WriteLine("Group Deleted");
+            }
+
+            Console.Read();
+        }
+    }
+}
